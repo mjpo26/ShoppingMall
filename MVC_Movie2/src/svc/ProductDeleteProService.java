f6359772fed944954cf5755962a0c744c10d0e85
ITWILL <ITWILL@ITWILLBS05-13>
2019-09-26 20:13:45 +0900

    삭제

---
+++ MVC_Movie2/src/svc/ProductDeleteProService.java
@@ -0,0 +1,33 @@
+package svc;
+
+import static db.JdbcUtil.commit;
+import static db.JdbcUtil.getConnection;
+import static db.JdbcUtil.rollback;
+
+import java.sql.Connection;
+
+import dao.ItemDAO;
+import dao.MemberDAO;
+
+public class ProductDeleteProService {
+
+	public boolean isDeleteItem(int item_code) {
+		boolean isDeleteSuccess = false;
+        
+        Connection con = getConnection();
+        ItemDAO itemDAO = ItemDAO.getInstance();
+        itemDAO.setConnection(con);
+        
+        int count = itemDAO.Delete(item_code);
+        if(count > 0) {
+            commit(con);
+            isDeleteSuccess = true;
+        } else {
+            rollback(con);
+        }
+        
+        return isDeleteSuccess;
+	}
+       
+    
+}
