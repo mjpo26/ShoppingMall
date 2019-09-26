6930afc147898686022a03ee6bd48525c69c9228
qhrud9556 <qhrud9556@naver.com>
2019-09-26 18:11:19 +0900

    2019.09.26 ㅇㅇ

--- MVC_Movie2/src/action/EventBoardListAction.java
+++ MVC_Movie2/src/action/EventBoardListAction.java
@@ -4,59 +4,51 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import svc.EventBoardListService;
+import svc.MemberInfoService;
+import svc.ReviewBoardDetailService;
+import svc.ReviewCommentDetailService;
 import vo.ActionForward;
+import vo.CommentBean;
 import vo.PageInfo;
+import vo.ReviewBoardBean;
 import vo.EventBean;
+import vo.MemberBean;
 
 public class EventBoardListAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ArrayList<EventBean> articleList = new ArrayList<EventBean>();
-        System.out.println("EventBoardListAction 실행됨");
-        // 페이징 처리를 위한 변수 선언
-        int page = 1; // 현재 페이지
-        int limit = 12; // 한 페이지 당 표시할 게시물 수
-        
-        // 파라미터로 전달된 page 파라미터가 null 이 아닐 경우 파라미터 값을 page 변수에 저장
-        if(request.getParameter("page") != null) {
-            page = Integer.parseInt(request.getParameter("page"));
-        }
-        
-        EventBoardListService boardListService = new EventBoardListService();
-        int listCount = boardListService.getListCount(); // 전체 게시물 수 가져오기
+		ArrayList<EventBean> articleList = new ArrayList<EventBean>();
+		HttpSession session = request.getSession();
 
-        articleList = boardListService.getArticleList(page, limit); // 전체 게시물 목록 가져오기(10개 한정)
-        
-        // 전체 페이지(마지막 페이지) 수 계산
-        int maxPage = (int)((double)listCount / limit + 0.95);
-        
-        // 시작 페이지 수 계산
-        int startPage = (((int)((double)page / 10 + 0.9)) - 1) * 10 + 1;
-        
-        // 끝 페이지 수 계산
-        int endPage = startPage + 10 - 1;
-        
-        // 끝 페이지 수가 전체 페이지 수 보다 클 경우 전체 페이지 수를 끝 페이지로 지정
-        if(endPage > maxPage) {
-            endPage = maxPage;
-        }
-        
-        // PageInfo 인스턴스 생성 후 페이징 처리 정보 저장
-        PageInfo pageInfo = new PageInfo(page, maxPage, startPage, endPage, listCount);
-        
-        // request 객체에 PageInfo 객체(pageInfo)와 ArrayList 객체(articleList)를 파라미터로 저장
-        request.setAttribute("pageInfo", pageInfo);
-        request.setAttribute("articleList", articleList);
-        
-        // ActionForward 객체를 생성하여 Dispatcher 방식으로 board 폴더 내의 qna_board_list.jsp 페이지로 이동
-        ActionForward forward = new ActionForward();
-        forward.setRedirect(false); // 생략 가능
-        forward.setPath("Main.ma");
-        
-        return forward;
+		int num = (Integer.parseInt(request.getParameter("review_num")));
+		System.out.println("대체뭔소리야 " + num);
+
+		EventBoardListService eventBoardListService = new EventBoardListService();
+		articleList = eventBoardListService.getArticleList(num);
+
+		int review_num = Integer.parseInt(request.getParameter("review_num"));
+		String page = request.getParameter("page");
+		String sId = (String) session.getAttribute("sId");
+		MemberBean memberBean = MemberInfoService.getMemberInfo(sId);
+		System.out.println(sId);
+
+		ReviewBoardDetailService boardDetailService = new ReviewBoardDetailService();
+
+		ReviewBoardBean article = boardDetailService.getArticle(review_num);
+
+		request.setAttribute("page", page);
+		request.setAttribute("article", article);
+		request.setAttribute("memberBean", memberBean);
+
+		ActionForward forward = new ActionForward();
+		forward.setPath("/reviewBoard/view.jsp");
+		forward.setRedirect(false);
+
+		return forward;
 	}
 
 }
--- MVC_Movie2/src/svc/EventBoardListService.java
+++ MVC_Movie2/src/svc/EventBoardListService.java
@@ -1,19 +1,27 @@
 package svc;
 
+import static db.JdbcUtil.close;
+import static db.JdbcUtil.getConnection;
+
+import java.sql.Connection;
 import java.util.ArrayList;
 
+import dao.ReviewBoardDAO;
 import vo.EventBean;
 
 public class EventBoardListService {
-
-	public int getListCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public ArrayList<EventBean> getArticleList(int page, int limit) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public ArrayList<EventBean> getArticleList(int num) throws Exception {
+        ArrayList<EventBean> articleList = null;
+        
+        Connection con = getConnection();  
+        ReviewBoardDAO mDAO = ReviewBoardDAO.getInstance();
+        mDAO.setConnection(con);
+        
+        articleList = mDAO.selectCommentList(num);
+        
+        close(con);
+        System.out.println("ReviewCommentDetailService의  ArrayList<CommentBean> 실행됨");
+        return articleList;
+    }
 
 }
